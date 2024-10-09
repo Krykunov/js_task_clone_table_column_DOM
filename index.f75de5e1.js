d3768eb0671e876ae3f7fdaf14ec1ccd1ec17ddd
Igor <4430704@gmail.com>
2024-10-09 12:29:03 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+const tableSrc = document.querySelector("table");
+const cloneColumn = (table, columnIndex)=>{
+    for (const el of [
+        ...table.children
+    ]){
+        const rows = [
+            ...el.children
+        ];
+        for (const row of rows){
+            const cellClone = row.children[columnIndex].cloneNode(true);
+            row.children[row.children.length - 1].before(cellClone);
+        }
+    }
+};
+cloneColumn(tableSrc, 1);
+
+//# sourceMappingURL=index.f75de5e1.js.map
